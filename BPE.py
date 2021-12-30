f2d17af8ea3283b35e5cd1b200f0b162e48d165b
Ben Fattori <ben.fattori@mail.utoronto.ca>
2021-12-30 12:43:08 -0500

    Linting.

--- BPE.py
+++ BPE.py
@@ -5,29 +5,38 @@ import nltk
 import pickle
 from tqdm import tqdm
 
-class BytePairEncoding():
+
+class BytePairEncoding:
     """
-    Class to perform Byte-Pair Encoding (BPE) on a givin corpus. 
+    Class to perform Byte-Pair Encoding (BPE) on a givin corpus.
 
     As outlined in 'Neural Machine Translation of Rare Words with Subword Units'
     <https://arxiv.org/abs/1508.07909>
-    
+
     """
 
-    def __init__(self, corpus_path, lower_case, EOW_TOKEN = '</w>', UNK_TOKEN = '<UNK>', PAD_TOKEN = '<PAD>'):
-        with open(corpus_path, encoding='utf-8') as f:
+    def __init__(
+        self,
+        corpus_path,
+        lower_case,
+        EOW_TOKEN="</w>",
+        UNK_TOKEN="<UNK>",
+        PAD_TOKEN="<PAD>",
+    ):
+        with open(corpus_path, encoding="utf-8") as f:
             self.corpus = f.read()
 
         if lower_case:
             self.corpus = self.corpus.lower()
 
         self.lower_case = lower_case
-        print(f'Base corpus has {len(self.corpus)} characters with {len(set(self.corpus))} distinct.')
+        print(
+            f"Base corpus has {len(self.corpus)} characters with {len(set(self.corpus))} distinct."
+        )
 
         self.EOW_TOKEN = EOW_TOKEN
         self.UNK_TOKEN = UNK_TOKEN
         self.PAD_TOKEN = PAD_TOKEN
-       
 
     # ---------- Preprocessing ---------- #
     def split_into_words_and_create_vocab(self):
@@ -50,7 +59,7 @@ class BytePairEncoding():
         return vocab
 
     # ---------- BPE Helper Functions ---------- #
-    def count_pairs(self,vocab):
+    def count_pairs(self, vocab):
         """
         From a vocabulary, count all pairs of symbols and return a sorted dict with all pair counts
 
@@ -68,11 +77,13 @@ class BytePairEncoding():
             # Iterate through each word and count pairs
             for idx in range(len(word.split()) - 1):
                 pairs["".join(word.split()[idx] + word.split()[idx + 1])] += freq
-                pairs_pattern[(word.split()[idx],word.split()[idx + 1])] += freq
+                pairs_pattern[(word.split()[idx], word.split()[idx + 1])] += freq
 
-        return dict(sorted(pairs.items(),  key=lambda x: x[1], reverse=True)), dict(sorted(pairs_pattern.items(),  key=lambda x: x[1], reverse=True))
+        return dict(sorted(pairs.items(), key=lambda x: x[1], reverse=True)), dict(
+            sorted(pairs_pattern.items(), key=lambda x: x[1], reverse=True)
+        )
 
-    def perform_merge(self,vocab, pairs, pairs_pattern):
+    def perform_merge(self, vocab, pairs, pairs_pattern):
         """
         From a (possibly merged) vocabulary, perform a merge on the given pattern
 
@@ -94,22 +105,22 @@ class BytePairEncoding():
         bigram = " ".join([pattern_A, pattern_B])
 
         # print(f'Best token: {pattern_find} -> {pattern}')
-        
+
         merged_vocab = collections.defaultdict(int)
 
         for word, freq in vocab.items():
 
-            #This regex case here is weird... had to look at the paper for the negative lookbehinds.
+            # This regex case here is weird... had to look at the paper for the negative lookbehinds.
             # Without this, for example, the merge rule ('e','l') would produce 'el','del','rel'
-            repl = re.sub(r'(?<!\S)' + re.escape(bigram) + r'(?!\S)', pattern, word)
+            repl = re.sub(r"(?<!\S)" + re.escape(bigram) + r"(?!\S)", pattern, word)
             merged_vocab[repl] += freq
 
         return merged_vocab, pattern
-       
-    def perform_BPE(self,num_merges):
+
+    def perform_BPE(self, num_merges):
 
         vocab = self.split_into_words_and_create_vocab()
-        #Create the base vocab of all symbols and progressively add to it 
+        # Create the base vocab of all symbols and progressively add to it
         self.vocab = self.create_vocab(vocab)
 
         for i in tqdm(range(num_merges)):
@@ -121,7 +132,7 @@ class BytePairEncoding():
 
         return vocab
 
-    def create_vocab(self,bpe_vocab):
+    def create_vocab(self, bpe_vocab):
         vocab = []
 
         for word, _ in bpe_vocab.items():
@@ -131,18 +142,18 @@ class BytePairEncoding():
         # return sorted(list(set(vocab)))
         return list(set(vocab))
 
-    def create_tokenization(self,vocab, save_pkl = True):
-        
-        itos = {i:vocab[i] for i in range(len(vocab))}
+    def create_tokenization(self, vocab, save_pkl=True):
 
-        stoi = {vocab[i]:i for i in range(len(vocab))}
+        itos = {i: vocab[i] for i in range(len(vocab))}
+
+        stoi = {vocab[i]: i for i in range(len(vocab))}
 
         # if save_pkl:
         #     pickle.dump(stoi,open(f'BPE/stoi_vocab_{len(chars)}.pkl', 'wb'))
         #     pickle.dump(itos,open(f'BPE/itos_vocab_{len(chars)}.pkl', 'wb'))
         return stoi, itos
 
-    def create_vocab_and_tokenization(self,num_merges):
+    def create_vocab_and_tokenization(self, num_merges):
         BPE_vocab = self.perform_BPE(num_merges=num_merges)
 
         tokens_stoi, tokens_itos = self.create_tokenization(self.vocab)
@@ -150,54 +161,65 @@ class BytePairEncoding():
         self.stoi = tokens_stoi
         self.itos = tokens_itos
 
-    
-    def tokenize(self,string_to_tokenize):
+    def tokenize(self, string_to_tokenize):
 
-        assert self.stoi is not None and self.itos is not None, 'Requires tokenization of base corpus first.'
+        assert (
+            self.stoi is not None and self.itos is not None
+        ), "Requires tokenization of base corpus first."
 
-        #Split string into characters and apply merge rules
+        # Split string into characters and apply merge rules
 
         split_chars = []
-        for word in nltk.wordpunct_tokenize(string_to_tokenize.lower() if self.lower_case else string_to_tokenize):
+        for word in nltk.wordpunct_tokenize(
+            string_to_tokenize.lower() if self.lower_case else string_to_tokenize
+        ):
             split_chars.append(" ".join(list(word) + [self.EOW_TOKEN]))
 
-       
         word_tokenization = []
-    
+
         for word in split_chars:
-            for token in sorted(self.vocab, key = len, reverse=True): 
-                #Splits BPE tokens like 'mathbb</w>' -> ['mathbb', '</w>', '']
-                split_tok = re.split(f'({self.EOW_TOKEN})', token)
+            for token in sorted(self.vocab, key=len, reverse=True):
+                # Splits BPE tokens like 'mathbb</w>' -> ['mathbb', '</w>', '']
+                split_tok = re.split(f"({self.EOW_TOKEN})", token)
                 if len(split_tok) > 1:
-                    pattern = " ".join(list(split_tok[0])+[split_tok[1]])
+                    pattern = " ".join(list(split_tok[0]) + [split_tok[1]])
                 else:
                     pattern = " ".join(list(split_tok[0]))
-                
-                #ugh.
-                if pattern == '\\':
-                    word = re.sub(r'(?<!\S)' + re.escape(pattern) + r'(?!\S)', repl=re.escape(token), string = word)
+
+                # ugh.
+                if pattern == "\\":
+                    word = re.sub(
+                        r"(?<!\S)" + re.escape(pattern) + r"(?!\S)",
+                        repl=re.escape(token),
+                        string=word,
+                    )
                 else:
-                    word = re.sub(r'(?<!\S)' + re.escape(pattern) + r'(?!\S)', repl=token, string = word)
+                    word = re.sub(
+                        r"(?<!\S)" + re.escape(pattern) + r"(?!\S)",
+                        repl=token,
+                        string=word,
+                    )
             word_tokenization += word.split()
-        # print(word_tokenization)
+        print(word_tokenization)
         return [self.stoi[i] for i in word_tokenization]
 
-    def tokens_to_str(self,tokens):
+    def tokens_to_str(self, tokens):
         """
         Takes a list of tokens and converts it back to text.
         """
-        
+
         concat_str = "".join([self.itos[i] for i in tokens])
 
         return " ".join(concat_str.split(self.EOW_TOKEN))
 
+
 if __name__ == "__main__":
 
-    BPE = BytePairEncoding(corpus_path= 'corpus.txt', lower_case = True)
+    BPE = BytePairEncoding(corpus_path="corpus.txt", lower_case=True)
 
-    BPE.create_vocab_and_tokenization(num_merges=100)
+    BPE.create_vocab_and_tokenization(num_merges=500)
 
-    tokens = BPE.tokenize(string_to_tokenize='This is a test sentence we are trying to tokenize. Lets see what happens. manifold Frobenius! Harry!')
-
+    tokens = BPE.tokenize(
+        string_to_tokenize="This is a test sentence we are trying to tokenize. Lets see what happens. manifold Frobenius! Harry!"
+    )
     print(BPE.tokens_to_str(tokens))
-
