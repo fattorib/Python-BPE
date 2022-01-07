9e71f650da7d953db3da3cd4a1067487869bbb67
Ben Fattori <ben.fattori@mail.utoronto.ca>
2022-01-06 21:26:29 -0500

    More tests + expected files

--- tests/tests.py
+++ tests/tests.py
@@ -1,11 +1,19 @@
 import unittest
 from BPE import BytePairEncoding
-
+import pickle
 
 class TestBPE(unittest.TestCase):
     def setUp(self) -> None:
         self.bpe = BytePairEncoding(
             corpus_path=r"tests\test_small.txt", lower_case=True
+        )
+
+        self.bpe_full_uncased = BytePairEncoding(
+            corpus_path=r"tests\test_medium.txt", lower_case=True
+        )
+
+        self.bpe_full_cased = BytePairEncoding(
+            corpus_path=r"tests\test_medium.txt", lower_case=False
         )
 
     def test_split_into_words_and_create_vocab(self):
@@ -30,24 +38,24 @@ class TestBPE(unittest.TestCase):
             "! </w>": 1,
         }
         pairs_expected = {
-            "is": 2,
-            "s</w>": 2,
-            "st": 2,
-            "th": 1,
-            "hi": 1,
-            "a</w>": 1,
-            "te": 1,
-            "es": 1,
-            "t</w>": 1,
-            "tr": 1,
-            "ri": 1,
-            "in": 1,
-            "ng": 1,
-            "g</w>": 1,
-            "!</w>": 1,
+            ("i", "s"): 2,
+            ("s", "</w>"): 2,
+            ("s", "t"): 2,
+            ("t", "h"): 1,
+            ("h", "i"): 1,
+            ("a", "</w>"): 1,
+            ("t", "e"): 1,
+            ("e", "s"): 1,
+            ("t", "</w>"): 1,
+            ("t", "r"): 1,
+            ("r", "i"): 1,
+            ("i", "n"): 1,
+            ("n", "g"): 1,
+            ("g", "</w>"): 1,
+            ("!", "</w>"): 1,
         }
 
-        pairs_bpe, _ = self.bpe.count_pairs(vocab)
+        pairs_bpe = self.bpe.count_pairs(vocab)
         self.assertEqual(pairs_expected, pairs_bpe)
 
     def test_perform_merge(self):
@@ -89,3 +97,22 @@ class TestBPE(unittest.TestCase):
         merged_vocab, _ = self.bpe.perform_merge(vocab, pairs_pattern)
 
         self.assertEqual(merged_vocab_expected, merged_vocab)
+
+    def test_perform_BPE_uncased(self):
+
+        self.bpe_full_uncased.create_vocab_and_tokenization(num_merges=250)
+
+        with open(r'tests\bpe_expected_uncased.pkl', 'rb') as f:
+            expected_vocab = pickle.load(f)
+        
+        self.assertEqual(set(expected_vocab), set(self.bpe_full_uncased.vocab))
+
+    def test_perform_BPE_cased(self):
+
+        self.bpe_full_cased.create_vocab_and_tokenization(num_merges=250)
+
+        with open(r'tests\bpe_expected_cased.pkl', 'rb') as f:
+            expected_vocab = pickle.load(f)
+        
+        self.assertEqual(set(expected_vocab), set(self.bpe_full_cased.vocab))
+
